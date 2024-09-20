4581b86c0c7549789eec40ec1ccc27ab8f35b6e8
Artem Suprun <efogdev@gmail.com>
2024-09-20 16:38:02 +0300

    Init

--- extension.js
+++ extension.js
@@ -1,82 +1,142 @@
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import { getInputSourceManager } from 'resource:///org/gnome/shell/ui/status/keyboard.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import Gio from 'gi://Gio'
+import GLib from 'gi://GLib'
+import GObject from 'gi://GObject'
+import St from 'gi://St'
+import Clutter from 'gi://Clutter'
+import * as Main from 'resource:///org/gnome/shell/ui/main.js'
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js'
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js'
 
-let panelButton;
+const DEFAULT_INTERVAL = 1 // seconds
+let instance = null
 
-const LangSwitchBtn = GObject.registerClass({
-}, class extends GObject.Object {
+async function* powertop(interval = DEFAULT_INTERVAL) {
+	const command = `script -qc "LINES=5 powertop -t ${interval} | cat -v | tr '|' '\n'"`
+
+	const proc = new Gio.Subprocess({
+		argv: [
+			'fakeroot',
+			'--',
+			'sh',
+			'-c',
+			command
+		],
+		flags: Gio.SubprocessFlags.STDOUT_PIPE,
+	})
+
+	proc.init(null)
+
+	const stdoutStream = new Gio.DataInputStream({
+		base_stream: proc.get_stdout_pipe(),
+		close_base_stream: true,
+	})
+
+	while (true) {
+		try {
+			const [ line ] = await stdoutStream.read_upto_async('', 0, GLib.PRIORITY_LOW, null)
+
+			if (line && line.trim()) {
+				if (/PowerTOP/m.test(line)) {
+					const [ _, dischargeRate ] = /discharge rate of (.+?) W/gim.exec(line)
+
+					yield dischargeRate
+				}
+			}
+		} catch (e) {
+			yield null
+		}
+	}
+}
+
+const BatteryDischargeIndicator = GObject.registerClass({}, class extends GObject.Object {
 	constructor() {
-		super();
-		this._signalId = 0;
-		this._keyboard = Main.panel.statusArea.keyboard;
-		this._inputSourceManager = getInputSourceManager();
+		super()
+
+		Gio._promisify(Gio.DataInputStream.prototype, 'read_upto_async', 'read_upto_finish')
+	}
+
+	async _watch() {
+		const readings = await powertop()
+
+		while (true) {
+			try {
+				const { value } = await readings.next()
+
+				if (value) {
+					this._addButton()
+					this._updateLabel(value)
+				} else {
+					this._removeButton()
+				}
+			} catch (e) {}
+		}
 	}
 
 	enable() {
-		this._keyboard.container.hide();
-		this._addButton();
+		this._checkDependencies()
+		this._addButton()
+		this._watch()
 	}
 
 	disable() {
-		this._keyboard.container.show();
-		this._removeButton();
+		this._removeButton()
 	}
 
 	_addButton() {
+		if (this._label && this._panelButton)
+			return
+
 		this._label = new St.Label({
 			text: '-',
 			x_expand: true,
 			y_expand: true,
 			y_align: Clutter.ActorAlign.CENTER,
-		});
-		this._signalId = this._inputSourceManager.connect('current-source-changed', this._updateLabel.bind(this));
-		this._updateLabel();
+		})
 
-		this._panelButton = new PanelMenu.Button(0.0, 'Language Switcher', false);
-		this._panelButton.add_child(this._label);
-		this._panelButton.connect('button-press-event', this._switchInputMethod.bind(this));
+		this._panelButton = new PanelMenu.Button(0.0, 'Battery discharge rate', false)
+		this._panelButton.add_child(this._label)
+		this._panelButton.connect('button-press-event', () => {})
 
-		// Use -1 with Dash to Panel
-		Main.panel.addToStatusArea('Indicator', this._panelButton, 0, 'right');
+		Main.panel.addToStatusArea('Indicator', this._panelButton, 0, 'right')
 	}
 
 	_removeButton() {
-		if (this._signalId) {
-			this._inputSourceManager.disconnect(this._signalId);
-			this._signalId = 0;
+		if (this._label) {
+			this._label.destroy()
+			this._label = null
 		}
 
-		this._label.destroy();
-		this._label = null;
-		this._panelButton.destroy();
-		this._panelButton = null;
+		if (this._panelButton) {
+			this._panelButton.destroy()
+			this._panelButton = null
+		}
 	}
 
-	_updateLabel() {
-		this._label.text = (this._inputSourceManager.currentSource.shortName).toUpperCase();
+	_updateLabel(value) {
+		if (!value || isNaN(value))
+			return
+
+		this._label.set_text(value.toString() + 'W')
 	}
 
-	_switchInputMethod() {
-		const numOfInputSources = Object.keys(this._inputSourceManager.inputSources).length;
-		const currentInput = this._inputSourceManager.currentSource;
-		const nextInput = this._inputSourceManager.inputSources[(currentInput.index + 1) % numOfInputSources];
-		nextInput.activate();
+	async _checkDependencies() {
+		try {
+			// const file = Gio.File.new_for_path('/usr/bin/powerpop')
+			// await file.query_info_async('standard::*,unix::uid', null, GLib.PRIORITY_DEFAULT, null)
+		} catch (e) {
+			console.log(e)
+		}
 	}
-});
+})
 
 export default class extends Extension {
 	enable() {
-		panelButton = new LangSwitchBtn();
-		panelButton.enable();
+		instance = new BatteryDischargeIndicator()
+		instance.enable()
 	}
 
 	disable() {
-		panelButton.disable();
-		panelButton = null;
+		instance.disable()
+		instance = null
 	}
 }
